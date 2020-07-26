cffe6ddae7e9a3e68fce1ff7a0850e55c5981d96
James Quick <james.q.quick@outlook.com>
2020-07-26 09:40:00 -0500

    TODOs in airtable helper functions

--- functions/courses.js
+++ functions/courses.js
@@ -4,5 +4,6 @@ const createCourse = require('./createCourse');
 const deleteCourse = require('./deleteCourse');
 const updateCourse = require('./updateCourse');
 exports.handler = async (event) => {
+    // TODO: call appropriate helper function based on HTTP method
     return formattedReturn(200, 'Hello World');
 };
--- functions/updateCourse.js
+++ functions/updateCourse.js
@@ -1,3 +1,5 @@
 const { table } = require('./airtable');
 const formattedReturn = require('./formattedReturn');
-module.exports = async (event) => {};
+module.exports = async (event) => {
+    // TODO: update course
+};
