c547e9a549293350df38dcc7b6d35fe3df7cab40
James Quick <james.q.quick@outlook.com>
2020-07-26 09:47:27 -0500

    Adding course layout

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
 export default function Course({ course, refreshCourses }) {
-    const markCoursePurchased = async () => {};
+    const markCoursePurchased = async () => {
+        //TODO mark course as purchased
+    };
 
-    const deleteCourse = async () => {};
-    return <div></div>;
+    const deleteCourse = async () => {
+        //TODO delete course
+    };
+    return (
+        <div className="list-group-item">
+            <a href={course.link}>
+                <h4 className="list-group-item-heading">{course.name}</h4>
+            </a>
+            <p className="list-group-item-text">A bunch of tags!</p>
+            <p>
+                Tags:{' '}
+                {course.tags &&
+                    course.tags.map((tag) => (
+                        <span className="badge badge-primary mr-2">{tag}</span>
+                    ))}
+            </p>
+            {!course.purchased && (
+                <button
+                    className="btn btn-sm btn-primary"
+                    onClick={markCoursePurchased}
+                >
+                    Purchased
+                </button>
+            )}
+            <button
+                className="btn btn-sm btn-danger ml-2"
+                onClick={deleteCourse}
+            >
+                Delete
+            </button>
+        </div>
+    );
 }
